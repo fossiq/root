eca03239317447340a879aecc14bee7eb72030f4
Sushruth Sastry <shastry.sushruth@gmail.com>
2025-12-10 00:38:01 -0800

    Switch test runner from Vitest to Bun in kql-lezer package

--- packages/kql-lezer/tests/comments.test.ts
+++ packages/kql-lezer/tests/comments.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect } from "bun:test";
 import { isValid, getTokens } from "./_helpers";
 
 describe("Comment parsing", () => {
--- packages/kql-lezer/tests/expressions.test.ts
+++ packages/kql-lezer/tests/expressions.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect } from "bun:test";
 import { isValid, getErrorCount } from "./_helpers";
 
 describe("Expression parsing", () => {
