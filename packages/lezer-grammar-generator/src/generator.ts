3635621ae86b2e50c819361f9f3f01ef8cf0aeb4
sushruth <shastry.sushruth@gmail.com>
2025-12-20 01:47:30 -0800

    refactor: remove barrel file from generator module
    - Remove src/generator/index.ts barrel file
    - Update src/generator.ts to import directly from specific modules
    - Maintains all functionality with direct imports
    - Follows AGENTS.md rule against barrel files

--- packages/lezer-grammar-generator/src/generator.ts
+++ packages/lezer-grammar-generator/src/generator.ts
@@ -1,1 +1,19 @@
-export * from "./generator/index.js";
+export {
+  generateGrammar,
+  generateGrammarFromPlugins,
+} from "./generator/main.js";
+export {
+  generateMacrosSection,
+  generateTokensSection,
+  generateRulesSection,
+  generatePrecedenceSection,
+  defaultTokenNames,
+} from "./generator/sections.js";
+export {
+  mergePlugins,
+  mergePrecedence,
+  rulesToAstTypes,
+  orderPlugins,
+} from "./generator/plugins.js";
+export { getStartRule, getCommentToken } from "./generator/utils.js";
+export { validateConfig } from "./generator/validation.js";
