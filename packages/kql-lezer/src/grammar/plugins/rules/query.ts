3121b2bb12a9d28758af29fab2b3eb14aea89c18
Sushruth Sastry <shastry.sushruth@gmail.com>
2026-01-21 13:30:57 -0800

    feat(kql-lezer): make QueryExpression optional in Query rule

--- packages/kql-lezer/src/grammar/plugins/rules/query.ts
+++ packages/kql-lezer/src/grammar/plugins/rules/query.ts
@@ -1,46 +1,82 @@
-import { seq, many, ref, choice, optional, type RuleDef, kw } from "@fossiq/lezer-grammar-generator";
+import {
+  seq,
+  many,
+  ref,
+  choice,
+  optional,
+  type RuleDef,
+  kw,
+} from "@fossiq/lezer-grammar-generator";
 
 /**
  * Top-level query rules.
  */
 export const queryRules: Record<string, RuleDef> = {
-    Query: {
-        expression: seq(
-            many(choice(ref("LetStatement"), ref("SetStatement"), ref("DeclareQueryParametersStatement"))),
-            ref("QueryExpression")
-        ),
-    },
-
-    QueryExpression: {
-        expression: choice(
-            ref("PipelineExpression"),
-            ref("UnionExpression"),
-            ref("SearchExpression"),
-            ref("FindExpression")
+  Query: {
+    expression: seq(
+      many(
+        choice(
+          ref("LetStatement"),
+          ref("SetStatement"),
+          ref("DeclareQueryParametersStatement")
         )
-    },
+      ),
+      optional(ref("QueryExpression"))
+    ),
+  },
 
-    UnionExpression: {
-        expression: seq(
-            kw("union"),
-            optional(ref("UnionParameters")),
-            ref("TableList")
-        )
-    },
+  QueryExpression: {
+    expression: choice(
+      ref("PipelineExpression"),
+      ref("UnionExpression"),
+      ref("SearchExpression"),
+      ref("FindExpression")
+    ),
+  },
 
-    SearchExpression: {
-        expression: seq(
-            kw("search"),
-            many(choice(ref("Identifier"), ref("String"), ref("Pipe"), ref("OpenParen"), ref("CloseParen"), kw("in"), kw("kind"), ref("Equals")))
-            // TODO: Improve search grammar
-        )
-    },
+  UnionExpression: {
+    expression: seq(
+      kw("union"),
+      optional(ref("UnionParameters")),
+      ref("TableList")
+    ),
+  },
 
-    FindExpression: {
-        expression: seq(
-            kw("find"),
-            many(choice(ref("Identifier"), ref("String"), ref("Pipe"), ref("OpenParen"), ref("CloseParen"), kw("in"), kw("kind"), ref("Equals")))
-            // TODO: Improve find grammar
+  SearchExpression: {
+    expression: seq(
+      kw("search"),
+      many(
+        choice(
+          ref("Identifier"),
+          ref("String"),
+          ref("Pipe"),
+          ref("OpenParen"),
+          ref("CloseParen"),
+          kw("in"),
+          kw("kind"),
+          ref("Equals")
         )
-    }
+      )
+      // TODO: Improve search grammar
+    ),
+  },
+
+  FindExpression: {
+    expression: seq(
+      kw("find"),
+      many(
+        choice(
+          ref("Identifier"),
+          ref("String"),
+          ref("Pipe"),
+          ref("OpenParen"),
+          ref("CloseParen"),
+          kw("in"),
+          kw("kind"),
+          ref("Equals")
+        )
+      )
+      // TODO: Improve find grammar
+    ),
+  },
 };
